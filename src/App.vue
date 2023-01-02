615d0607682bb9c6106eba2ef017db88e4de2dd0
student <shakedgo@gmail.com>
2023-01-02 19:44:54 +0200

    Added Vue-Tutorial

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<h2>{{ title }}</h2>
+	<input type="text" ref="inputRef" />
+	<!-- <button @click="handleClick">Focus</button> -->
+	<button @click="setText">Change Text</button>
+	<Banner v-if="showBanner" :title="text + ' 1'" @close="toggleBanner" />
+	<Banner v-if="showBanner" :title="text + ' 2'" @close="toggleBanner" />
+	<BannerSlot v-if="showBanner">
+		<div>Slot Banner {{ text + " 3" }}</div>
+		<template v-slot:btn>
+			<button @click="toggleBanner">Don't show Banners</button>
+		</template>
+	</BannerSlot>
+	<button v-else @click="toggleBanner">Show Banners</button>
+</template>
+
+<script>
+import Banner from "./components/Banner.vue";
+import BannerSlot from "./components/BannerSlot.vue";
+
+export default {
+	data() {
+		return {
+			title: "Hello World",
+			text: "",
+			showBanner: true,
+		};
+	},
+	components: { Banner, BannerSlot },
+	methods: {
+		handleClick() {
+			this.$refs.inputRef.focus();
+		},
+		setText() {
+			this.text = this.$refs.inputRef.value;
+			console.log(this.text);
+		},
+		toggleBanner() {
+			this.showBanner = !this.showBanner;
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="container">
+		<div>Banner {{ title }}</div>
+		<button @click="toggle">Don't show Banners</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["title"],
+	methods: {
+		toggle() {
+			this.$emit("close"); // listening to close on parent component
+			// On parent component: @close="toggleBanners"
+		},
+	},
+};
+</script>
+
+<style scoped>
+.container {
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+	justify-content: center;
+	align-items: center;
+	margin: 5px;
+	border: 1px solid black;
+	border-radius: 8px;
+}
+</style>
