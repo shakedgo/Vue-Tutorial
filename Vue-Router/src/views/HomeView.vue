ba795c90f9d429aa57298f70e7a0b2344407f2a8
student <shakedgo@gmail.com>
2023-01-03 15:48:44 +0200

    Cleaned Vue-Router

--- Vue-Router/src/views/HomeView.vue
+++ Vue-Router/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+	<h1>Home</h1>
+</template>
+
+<script>
+export default {};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style></style>
